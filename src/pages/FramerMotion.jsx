6a461085024c9a3a28af0b846f166275f0ce4934
JullianMQ <jullianmq@gmail.com>
2024-12-07 01:13:53 +0800

    moved some elements as components

--- src/pages/FramerMotion.jsx
+++ src/pages/FramerMotion.jsx
@@ -22,25 +22,37 @@ const FramerMotion = () => {
                 </div>
             </div>
 
+            <AnimateOnceInView />
+            <AnimateAlwaysInView />
+        </>
+    )
+}
+// TODO: MAKE AN ACCORDION WITH FRAMER MOTION
+
+const AnimateOnceInView = () => {
+    return (
             <div className="h-[60svh] bg-green-200 p-8">
                 <motion.h1 className="text-5xl font-bold"
-                    initial={{opacity: 0, translateX: -500}}
-                    whileInView={{opacity: 1, translateX: 0}}
+                    initial={{ opacity: 0, translateX: -500 }}
+                    whileInView={{ opacity: 1, translateX: 0 }}
                     viewport={{ once: true }}
                 >
                     Animate once in view
                 </motion.h1>
             </div>
+    )
+}
+
+const AnimateAlwaysInView = () => {
+    return (
             <div className="h-[60svh] bg-red-200 p-8">
                 <motion.h1 className="text-5xl font-bold"
-                    initial={{opacity: 0, translateX: -500}}
-                    whileInView={{opacity: 1, translateX: 0}}
+                    initial={{ opacity: 0, translateX: -500 }}
+                    whileInView={{ opacity: 1, translateX: 0 }}
                 >
                     Animate always in view
                 </motion.h1>
             </div>
-
-        </>
     )
 }
 
