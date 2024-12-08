2728aa0eda19937335dce57a5aee546041ff7b44
JullianMQ <jullianmq@gmail.com>
2024-12-08 16:09:13 +0800

    Added the side navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,42 @@
 // import React from 'react'
 import '../output.css'
 import { Link } from 'react-router-dom'
+import { motion } from 'motion/react'
+import { useState } from 'react'
 
 const Navbar = () => {
-    return (
+    const [isOpen, setIsOpen] = useState(false)
 
-        // TODO: MAKE A NEW NAVBAR THAT WILL USE AUTO ANIMATE LIBRARY
-        <nav className="grid justify-center p-2 bg-sky-800 text-white mb-8">
-            <ul className="flex gap-8 font-semibold">
-                <Link to="/">Home</Link>
-                <Link to="/tictactoe">TicTacToe</Link>
-                <Link to="/thinkinreact">Thinking in React</Link>
-                <Link to="/autoanimate">AutoAnimate</Link>
-                <Link to="/framermotion">FramerMotion</Link>
-            </ul>
-        </nav>
+    return (
+        <>
+            <div className="w-screen">
+                <nav className="grid grid-cols-3 grid-rows-none bg-gray-500 text-white p-2">
+                    <p className="col-start-2 col-end-2 justify-self-center font-bold">
+                        LEARNINGS IN REACT AND OTHER LIBRARIES
+                    </p>
+                    <button className="col-start-3 col-end-3 justify-self-end mr-4 hover:scale-105"
+                        onClick={() => setIsOpen(!isOpen)}>
+                        <svg className="size-6"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </button>
+                </nav>
+
+                <motion.section className="h-lvh bg-gray-500 absolute right-0 text-nowrap overflow-x-hidden"
+                    initial={{ width: 0 }} animate={isOpen ? { width: 200, padding: 8 } : { width: 0, padding: 0 }} 
+                    transition={{ type: 'spring', stiffness: 500, damping: 40, duration: .3}}>
+
+                    <ul className="text-white font-bold flex flex-col gap-2 overflow-x-hidden">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/tictactoe">TicTacToe</Link></li>
+                        <li><Link to="/thinkinreact">Thinking in React</Link></li>
+                        <li><Link to="/autoanimate">AutoAnimate</Link></li>
+                        <li><Link to="/framermotion">FramerMotion</Link></li>
+                    </ul>
+                </motion.section>
+            </div>
+        </>
     )
 }
 
--- src/pages/FramerMotion.jsx
+++ src/pages/FramerMotion.jsx
@@ -71,8 +71,8 @@ const Accordion = () => {
                 onClick={() => setIsOpen(!isOpen)}>
                 <div className="flex justify-between">
                     <p className="">Accordion here</p>
-                    {isOpen ? <p className="text-xs align-text-bottom">&#8897;</p> :
-                        <p className="text-xs align-text-bottom">&#8896;</p> }
+                    {isOpen ? <p className="text-xs align-text-bottom"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"> <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" /> </svg> </p> :
+                        <p className="text-xs align-text-bottom"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"> <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" /> </svg> </p> }
 
 
                 </div>
